4ebd71c17c857176e5773bc72b7ffd517a3bdb5c
Hassan AKAR <hassan.akar@outlook.fr>
2017-12-27 14:32:36 +0100

    Add indexAction

--- src/AppBundle/Controller/PokemonController.php
+++ src/AppBundle/Controller/PokemonController.php
@@ -11,9 +11,21 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
 
 
 class PokemonController extends Controller
 {
+
+    /**
+     * @Route("/pokemon", name="pokemon_list")
+     * @Method("GET")
+     */
+    public function indexAction(Request $request)
+    {
+        return $this->render('pokemon/index.html.twig');
+
+    }
 
 }
